e12cc98b90e1ad468b979260ac4fc4a5414b6c90
uelintond <uelintond25@gmail.com>
2024-03-18 20:32:18 -0300

    Primeiro commit: Bot Telegram para execução de comandos SSH em servidores remotos

---
+++ README.md
@@ -0,0 +1,54 @@
+# Bot de Comandos SSH para Servidores Remotos
+Este é um bot Telegram desenvolvido em Python que permite executar comandos SSH em servidores remotos acessíveis via SSH. O bot utiliza a biblioteca paramiko para estabelecer conexões SSH seguras e confiáveis.
+
+Recursos Principais:
+Execução de Comandos SSH: Com este bot, é possível executar comandos SSH em servidores remotos de forma remota e segura.
+## Como Utilizar:
+- Configuração:
+Clone este repositório para o seu ambiente local:
+
+```
+git clone https://github.com/seuusuario/seurepositorio.git
+```
+Instale as dependências necessárias usando pip:
+```
+pip install -r requirements.txt
+```
+
+Edite o arquivo main.py para adicionar as informações do seu bot Telegram e do servidor remoto:
+
+# Defina as informações do seu bot Telegram:
+
+## Defina as informações do seu bot
+```
+TOKEN = 'SEU_TOKEN_AQUI'
+bot = telebot.TeleBot(TOKEN)
+```
+## Defina o seu ID de chat
+CHAT_ID = 0000000000
+Defina as informações do servidor remoto (substitua HOST, PORT, USERNAME, PRIVATE_KEY ou PASSWORD pelos detalhes do seu servidor):
+
+
+## Defina as informações do servidor remoto
+```
+HOST = 'IP_DO_SEU_SERVIDOR'
+PORT = 22
+USERNAME = 'USERNAME_DO_SEU_SERVIDOR'
+PRIVATE_KEY = '/caminho/para/sua/chave_privada'  # ou deixe em branco se estiver usando senha
+PASSWORD = 'SENHA_DA_CHAVE_PRIVADA'  # Deixe em branco se você não definiu uma senha para a chave privada
+```
+
+## Adicionando Novos Comandos:
+
+- Você pode adicionar novos comandos ou ajustar os existentes no arquivo main.py. Cada comando é tratado em uma função separada no código Python.
+
+Para adicionar um novo comando, crie uma nova função de manipulador de mensagem, semelhante às existentes, e registre-a usando @bot.message_handler(commands=['seu_comando']).
+### Notas Importantes:
+- Certifique-se de ter permissões adequadas para executar os comandos SSH no servidor remoto.
+- Mantenha suas informações de autenticação (chave privada ou senha) em segurança. 
+- Não compartilhe-as publicamente.
+
+Este bot é apenas uma ferramenta para facilitar operações remotas em servidores SSH e deve ser usado com responsabilidade.
+
+### Contribuições e Melhorias:
+Se você tiver sugestões, correções ou melhorias para este bot, sinta-se à vontade para contribuir! Basta enviar um pull request com suas alterações.
---
+++ main.py
@@ -0,0 +1,89 @@
+import paramiko
+import telebot
+
+# Defina as informações do seu bot
+TOKEN = 'SEU_TOKEN_AQUI'
+bot = telebot.TeleBot(TOKEN)
+
+
+CHAT_ID = 0000000000  # Defina o seu ID de chat
+
+HOST = 'IP_DO_SEU_SERVIDOR'  # Defina as informações do servidor Linode
+PORT = 22
+USERNAME = 'USERNAME_DO_SEU_SERVIDOR'
+PRIVATE_KEY = '/caminho/para/sua/chave_privada' # Exemplo ppk: /root/manager/id_rsa.ppk
+PASSWORD = 'SENHA_DA_CHAVE_PRIVADA' # Deixe em branco se você não definiu uma senha para a chave privada
+
+# Função para lidar com o comando SSH e reiniciar o servidor
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.chat.id == CHAT_ID:
+        bot.reply_to(
+            message, "O que você deseja fazer? \n\n /restart_server para reiniciar o servidor Linode\n /restart_check para reiniciar o serviço GRD Check.")
+    else:
+        bot.reply_to(
+            message, "Acesso negado. Você não tem permissão para executar este comando.")
+
+# Função para lidar com o comando SSH e reiniciar o servidor
+@bot.message_handler(commands=['restart_server'])
+def restart_server(message):
+    try:
+        # Verifica se a mensagem foi enviada por você
+        if message.chat.id == CHAT_ID:
+            # Conecte-se ao servidor Linode com a chave privada e senha
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(HOST, PORT, USERNAME,
+                        key_filename=PRIVATE_KEY, password=PASSWORD)
+
+            # Execute o comando SSH para reiniciar o serviço
+            command = 'sudo systemctl reboot'
+            stdin, stdout, stderr = ssh.exec_command(command)           
+            output = stdout.read().decode()  # Obtenha a saída do comando
+            ssh.close() # Fecha a conexão SSH
+
+            # Verifica se a saída está vazia
+            if not output.strip():
+                bot.reply_to(
+                    message, "O comando foi executado com sucesso. O servidor foi reiniciado.")
+            else:
+                bot.reply_to(message, output)
+        else:
+            bot.reply_to(
+                message, "Você não tem permissão para executar este comando.")
+    except Exception as e:
+        bot.reply_to(
+            message, f"Ocorreu um erro ao executar o comando SSH: {str(e)}")
+
+# Função para lidar com o comando SSH e reiniciar o serviço
+@bot.message_handler(commands=['restart_check'])
+def restart_check(message):
+    try:
+        # Verifica se a mensagem foi enviada por você
+        if message.chat.id == CHAT_ID:
+            # Conecta ao servidor Linode com a chave privada e senha
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(HOST, PORT, USERNAME,
+                        key_filename=PRIVATE_KEY, password=PASSWORD)
+
+            # Executa o comando SSH para reiniciar o serviço
+            command = 'sudo systemctl restart grdcheck.service'
+            stdin, stdout, stderr = ssh.exec_command(command)
+            output = stdout.read().decode() # Obtenha a saída do comando
+            ssh.close() # Feche a conexão SSH
+
+            # Verifica se a saída está vazia
+            if not output.strip():
+                bot.reply_to(
+                    message, "O comando foi executado com sucesso.")
+            else:
+                bot.reply_to(message, output)
+        else:
+            bot.reply_to(
+                message, "Você não tem permissão para executar este comando.")
+    except Exception as e:
+        bot.reply_to(
+            message, f"Ocorreu um erro ao executar o comando SSH: {str(e)}")
+        
+bot.polling()
